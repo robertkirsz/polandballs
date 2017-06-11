e1a006704ec963a447912761274df611975cefd8
Robert Kirsz <robert.kirsz@gmail.com>
2017-06-11 23:23:52 +0200

    Added a comment and some space to social buttons

--- src/styled/SocialButton.js
+++ src/styled/SocialButton.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
 import IconButton from 'material-ui/IconButton'
 
+// !important is needed here due to the way Material UI specificity works
+
 const SocialButton = styled(IconButton)`
   width: 30px !important;
   height: 30px !important;
+  margin-bottom: 4px;
+
   img {
     width: 17px;
     height: 17px;
